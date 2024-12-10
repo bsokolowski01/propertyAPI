fc3bdba8767b41e5db9c87acd6c27a4ecff4928f
bsokolowski <s24531@pjwstk.edu.pl>
2024-12-10 13:05:18 +0100

    implement filter, sort and pagination

--- grpc/services/filters.js
+++ grpc/services/filters.js
@@ -1,11 +1,24 @@
 export const filter = (records, filters) => {
-    return records.filter(record => {
-      return filters.every(filter => {
-        return record[filter.field] === filter.value;
-      });
-    });
-  }
+  return records.filter(record => {
+    return filters.every(filter => {
+      if(filter.eq){
+        return record[filter.field] === filter.eq
+      }
+      else if(filter.neq){
+        return record[filter.field] !== filter.neq
+      }
+      else if(filter.contains){
+        return record[filter.field].includes(filter.contains)
+      }
+      else if(filter.notContains){
+        return !record[filter.field].includes(filter.notContains)
+      }
+      else return false;
+    }); 
+  });
+}
   
+
 export const sort = (records, sorts) => {
     return records.sort((a, b) => {
       for (const sort of sorts) {
